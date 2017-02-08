137e71f1edde2c41414c684de872cd55c33a825a
hattori6789 <hattori.stdio@gmail.com>
2017-02-08 19:34:51 +0900

    接続元に応じたロケールの判断

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,22 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   before_action :restore_locale
+  before_action :set_locale
 
   def url_options
     { :locale => I18n.locale }.merge(super)
   end
 
   private
+
+  def set_locale
+    extracted_locale = extract_locale_from_accept_language
+    I18n.locale = (I18n::available_locales.include? extracted_locale.to_sym) ? extracted_locale : I18n.default_locale
+  end
+
+  def extract_locale_from_accept_language
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
 
   def restore_locale
     I18n.locale = params[:locale] if params[:locale]
