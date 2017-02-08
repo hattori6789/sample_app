bebed23c6485f141bd61624ba884e28976685db6
hattori6789 <hattori.stdio@gmail.com>
2017-02-08 17:19:48 +0900

    ロケール値が明示的に指定されていたらそのロケールで表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  before_action :restore_locale
+
+  def url_options
+    { :locale => I18n.locale }.merge(super)
+  end
 
   private
+
+  def restore_locale
+    I18n.locale = params[:locale] if params[:locale]
+  end
 
   def logged_in_user
     unless logged_in?
