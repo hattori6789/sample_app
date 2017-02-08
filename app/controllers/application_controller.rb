2714ec7940e2b4a4d5b929a831b15303a2d71ff6
hattori6789 <hattori.stdio@gmail.com>
2017-02-08 20:26:42 +0900

    IP アドレスで判断

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,20 @@
 class ApplicationController < ActionController::Base
+  require 'geoip'
   protect_from_forgery with: :exception
   include SessionsHelper
-  before_action :restore_locale
   before_action :set_locale
-
-  def url_options
-    { :locale => I18n.locale }.merge(super)
-  end
 
   private
 
   def set_locale
-    extracted_locale = extract_locale_from_accept_language
+    extracted_locale = extract_locale_from_ip
     I18n.locale = (I18n::available_locales.include? extracted_locale.to_sym) ? extracted_locale : I18n.default_locale
   end
 
-  def extract_locale_from_accept_language
-    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
-  end
-
-  def restore_locale
-    I18n.locale = params[:locale] if params[:locale]
+  def extract_locale_from_ip
+    @geoip ||= GeoIP.new(Rails.root.join("lib/GeoIP.dat"))
+    country_location = @geoip.country(request.remote_ip)
+    country_location.country_code2.downcase
   end
 
   def logged_in_user
