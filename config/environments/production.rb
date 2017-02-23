4bd1c423dd6bdfeca25aa76e57ee82e04a001d5a
hattori6789 <hattori.stdio@gmail.com>
2017-02-23 14:10:11 +0900

    i18n on kaminari

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://shrouded-ravine-67538.herokuapp.com'
+  host = 'https://boiling-springs-10878.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
